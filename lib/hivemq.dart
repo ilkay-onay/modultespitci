10d3e86812b02ff97d2138aa40cc4bac0a537130
ilkay-onay <ilkayonay2001@gmail.com>
2024-05-03 10:58:26 +0300

    localization, handshake, better stock image

--- lib/hivemq.dart
+++ lib/hivemq.dart
@@ -1,10 +1,10 @@
 import 'dart:io';
 import 'dart:convert';
 import 'main.dart';
+import 'dart:async'; 
 import 'package:mqtt_client/mqtt_client.dart';
 import 'package:mqtt_client/mqtt_server_client.dart';
 
-// connection states for easy identification
 enum MqttCurrentConnectionState {
   IDLE,
   CONNECTING,
@@ -25,7 +25,6 @@ class MQTTClientWrapper {
   MqttCurrentConnectionState connectionState = MqttCurrentConnectionState.IDLE;
   MqttSubscriptionState subscriptionState = MqttSubscriptionState.IDLE;
 
-  // using async tasks, so the connection won't hinder the code flow
   void prepareMqttClient() async {
     _setupMqttClient();
     await _connectClient();
@@ -33,25 +32,23 @@ class MQTTClientWrapper {
     publishMessage('hello1');
   }
 
-  // waiting for the connection, if an error occurs, print it and disconnect
   Future<void> _connectClient() async {
     try {
-      print('client connecting....');
+      print('client baglaniyor...');
       connectionState = MqttCurrentConnectionState.CONNECTING;
       await client.connect('cokgucluisim', 'cokguclubirsifre');
     } on Exception catch (e) {
-      print('client exception - $e');
+      print('client hata - $e');
       connectionState = MqttCurrentConnectionState.ERROR_WHEN_CONNECTING;
       client.disconnect();
     }
 
-    // when connected, print a confirmation, else print an error
     if (client.connectionStatus!.state == MqttConnectionState.connected) {
       connectionState = MqttCurrentConnectionState.CONNECTED;
-      print('client connected');
+      print('client baglandi');
     } else {
       print(
-          'ERROR client connection failed - disconnecting, status is ${client.connectionStatus}');
+          'hata client baglantisi basarisiz - baglanti kesiliyor, durum ${client.connectionStatus}');
       connectionState = MqttCurrentConnectionState.ERROR_WHEN_CONNECTING;
       client.disconnect();
     }
@@ -59,7 +56,6 @@ class MQTTClientWrapper {
 
   void _setupMqttClient() {
     client = MqttServerClient.withPort('12b10b214cce489e91869af533703219.s1.eu.hivemq.cloud', 'flutter', 8883);
-    // the next 2 lines are necessary to connect with tls, which is used by HiveMQ Cloud
     client.secure = true;
     client.securityContext = SecurityContext.defaultContext;
     client.keepAlivePeriod = 20;
@@ -67,26 +63,40 @@ class MQTTClientWrapper {
     client.onConnected = _onConnected;
     client.onSubscribed = _onSubscribed;
   }
+  
 
   void _subscribeToTopic(String topicName) {
-    print('Subscribing to the $topicName topic');
+    print(' $topicName basligina abone olunuyor');
     client.subscribe(topicName, MqttQos.atMostOnce);
 
-    // print the message when it is received
     client.updates!.listen((List<MqttReceivedMessage<MqttMessage>> c) {
       final MqttPublishMessage recMess = c[0].payload as MqttPublishMessage;
       var message = MqttPublishPayload.bytesToStringAsString(recMess.payload.message);
 
-      print('YOU GOT A NEW MESSAGE:');
+      print('Yeni bir mesaj geldi:');
       print(message);
       if(message == "fallen")
         isFallDetected = true;
       else if(message == "kapali"){
         isRaspiConnected = true;
-        isAlarmActive = false;}
+        isAlarmActive = false;
+        timer3?.cancel();
+        timer3 = Timer.periodic(Duration(seconds: 150), (Timer t) {
+        isRaspiConnected = false;
+  });
+        
+        }
+    
       else if(message == "acik"){
         isRaspiConnected = true;
-        isAlarmActive = true;}
+        isAlarmActive = true;
+        timer3?.cancel();
+        timer3 = Timer.periodic(Duration(seconds: 150), (Timer t) {
+        isRaspiConnected = false;
+  });
+        
+        
+        }
 
       else
       {
@@ -103,24 +113,23 @@ class MQTTClientWrapper {
     final MqttClientPayloadBuilder builder = MqttClientPayloadBuilder();
     builder.addString(message);
 
-    print('Publishing message "$message" to topic ${'fall_detection/alert'}');
+    print('Mesaj gonderiliyor: "$message" Su basliga: ${'fall_detection/alert'}');
     client.publishMessage('fall_detection/alert', MqttQos.exactlyOnce, builder.payload!);
   }
 
-  // callbacks for different events
   void _onSubscribed(String topic) {
-    print('Subscription confirmed for topic $topic');
+    print('Abone dogrulandi $topic');
     subscriptionState = MqttSubscriptionState.SUBSCRIBED;
   }
 
   void _onDisconnected() {
-    print('OnDisconnected client callback - Client disconnection');
+    print('client baglantisi kesildi');
     connectionState = MqttCurrentConnectionState.DISCONNECTED;
   }
 
   void _onConnected() {
     connectionState = MqttCurrentConnectionState.CONNECTED;
-    print('OnConnected client callback - Client connection was sucessful');
+    print('client baglantisi basarili');
   }
 
 }
